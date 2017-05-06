3c6b00c5e6f246f4949f882617814a5b7dedda98
rav <s3gm3nt4ti0nf4ult@users.noreply.github.com>
2017-05-07 00:31:45 +0200

    Pylint rating 10/10, optimization flag, cleanup

--- README.md
+++ README.md
@@ -1,12 +1,51 @@
 # image-binder
-This easy script merges multiple images into one. Only 2x2, 4x4,... 
+This easy script merges multiple images into one. For example 2x1 box or 4x4 etc...
 
 
-###### Usage:
-  `python bind.py box_size(x)xbox_size(y) inputfile1, inputfile2, ...`
 
-Where box_size(x) is a number of columns and box_size(y) is a number of rows. So if you want merge 4 files into 2 rows and 2 columns just use 2x2 as parameter (without spaces).
+### Usage:
+###### Help:
+To get help just type:
 
+  `python bind.py -h`
 
-TODO:
-- [ ] fill blank with defined colour
+Commands:
+```
+
+python bind.py bind.py -h
+
+     _                                  _     _           _
+    (_)_ __ ___   __ _  __ _  ___      | |__ (_)_ __   __| | ___ _ __
+    | | '_ ` _ \ / _` |/ _` |/ _ \_____| '_ \| | '_ \ / _` |/ _ \ '__|
+    | | | | | | | (_| | (_| |  __/_____| |_) | | | | | (_| |  __/ |
+    |_|_| |_| |_|\__,_|\__, |\___|     |_.__/|_|_| |_|\__,_|\___|_|
+                       |___/
+                   
+usage: python binder.py [-h] -sx N -sy M -i [img1, img2...] -o output_img.tiff
+                    [-opt True]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -sx N, --sizex N      Horizontal images number
+  -sy M, --sizey M      Vertical images number
+  -i [img1, img2...], --input [img1, img2...]
+                        Input images to be binded
+  -o output_img.tiff, --output output_img.tiff
+                        Output image
+  -opt True, --optimize True
+                        Saved file optimization
+
+Bye!
+
+```
+
+`-sx` and `-sy` specifies the size of output 'image box'. For example `-sx 2 -sy 2` means that your output image will contain 4 images and have size of 2 images vertically and 2 images horizontally.
+
+`-opt` or `-optimize` enables `optimize` flag in PIL, so the size of ouput image will be reduced. 
+
+I wanted to make it 100% 10/10 rated in pylint :).
+
+### TODO:
+- [x] Argparse argument parsing
+- [ ] filling blank places (for example 3 image box) with predefined color
+- [ ] TBD
--- bind.py
+++ bind.py
@@ -1,9 +1,12 @@
+
+'''
+    Image-binder is a very simple tool designed to combine multiple images
+    for example data charts or diagrams, side by side into single image
+'''
+
 import sys
 import argparse
 from PIL import Image
-
-
-
 #def check_box_size(l):
 #    if len(l) == 1:
 #        try:
@@ -41,85 +44,102 @@ def img_sizer(img_list):
         sys.exit(1)
     print("Largest image size {} px x {} px\n".format(x_s, y_s))
 
-    return x_s, y_s 
+    return x_s, y_s
 
 
 
-def resize(img_list, x, y):
+def resize(img_list, size_x, size_y):
     '''resizes to the biggest image size found by img_size()'''
     for i in img_list:
         try:
             img = Image.open(i)
-            if img.size != (x, y):
+            if img.size != (size_x, size_y):
                 print("Resizing {}".format(i))
-                img = img.resize((x, y), Image.ANTIALIAS)
+                img = img.resize((size_x, size_y), Image.ANTIALIAS)
                 img.save(i)
             else:
                 pass
         except (FileNotFoundError, IOError, ValueError) as error:
-            print('Error while processing {}\nOccured:{}'.format(i, error))
+            print('Error while processing {} file occured. Exiting:\n{}'.format(i, error))
 
 
 
-def make_image(box_x, box_y, x, y, images):
+def make_image(box_x, box_y, ppi_x, ppi_y, images):
     '''does the core of binding'''
 
-    image_s_x = box_x * x
-    image_s_y = box_y * y
-    print('{} x {}\n{} px x {} px'.format(x, y, image_s_x, image_s_y))
+    image_s_x = box_x * ppi_x
+    image_s_y = box_y * ppi_y
+
+    print('{} x {}\n{} px x {} px'.format(ppi_x, ppi_x, image_s_x, image_s_y))
+
     pos_x, pos_y = 0, 0
     result = Image.new('RGB', (image_s_x, image_s_y))
     for i in images:
         print('Pasting {} in {} x {} '.format(i, pos_x, pos_y))
         try:
             img = Image.open(i)
-        except:
-            print('Cannot open file {}.'.format(i))
-        result.paste(img, (pos_x, pos_y, pos_x+x, pos_y+y))
+            result.paste(img, (pos_x, pos_y, pos_x+ppi_x, pos_y+ppi_y))
+        except (FileNotFoundError, IOError, ValueError) as error:
+            print('Error while processing {} file occured. Exiting:\n{}'.format(i, error))
+            sys.exit(1)
         if (images.index(i) + 1) % box_x == 0:
             pos_x = 0
-            pos_y += y
+            pos_y += ppi_y
         elif images.index(i) == 0 or images.index(i)+1 % box_x != 0:
-            pos_x += x
-    
+            pos_x += ppi_x
+
     return result
-  
-  
+
+
 
 
 def hello():
     '''Function displays hello prompt'''
     print(r'''
-     _                                  _     _           _           
-    (_)_ __ ___   __ _  __ _  ___      | |__ (_)_ __   __| | ___ _ __ 
+     _                                  _     _           _
+    (_)_ __ ___   __ _  __ _  ___      | |__ (_)_ __   __| | ___ _ __
     | | '_ ` _ \ / _` |/ _` |/ _ \_____| '_ \| | '_ \ / _` |/ _ \ '__|
-    | | | | | | | (_| | (_| |  __/_____| |_) | | | | | (_| |  __/ |   
-    |_|_| |_| |_|\__,_|\__, |\___|     |_.__/|_|_| |_|\__,_|\___|_|   
-                       |___/                                          
+    | | | | | | | (_| | (_| |  __/_____| |_) | | | | | (_| |  __/ |
+    |_|_| |_| |_|\__,_|\__, |\___|     |_.__/|_|_| |_|\__,_|\___|_|
+                       |___/
                    ''')
 
 
 def main(argv):
     '''main function'''
-    
-    
-    parser = argparse.ArgumentParser(prog='image-binder', add_help=True, description=hello(), epilog='Bye!')
-    parser.add_argument('-sx', '--sizex', metavar='N', required=True, type=int, help='Horizontal images number')
-    parser.add_argument('-sy', '--sizey', metavar='M', required=True, type=int, help='Vertical images number')
-    parser.add_argument('-i', '--input', metavar='img1, img2...', required=True, type=str, nargs='?', help='Input images to be binded')
-    parser.add_argument('-o', '--output', metavar='output_img.tiff', required=True, type=str, help='Output image')
+
+
+    parser = argparse.ArgumentParser(prog=argv[0], add_help=True,
+                                     description=hello(), epilog='Bye!')
+
+    parser.add_argument('-sx', '--sizex', metavar='N', required=True,
+                        type=int, help='Horizontal images number')
+
+    parser.add_argument('-sy', '--sizey', metavar='M', required=True,
+                        type=int, help='Vertical images number')
+
+    parser.add_argument('-i', '--input', metavar='img1, img2...',
+                        required=True, type=str, nargs='?', help='Input images to be binded')
+
+    parser.add_argument('-o', '--output', metavar='output_img.tiff',
+                        required=True, type=str, help='Output image')
+
+    parser.add_argument('-opt', '--optimize', metavar='True', default=True,
+                        required=False, type=bool, help='Saved file optimization')
+
     arguments = parser.parse_args()
 
     blok_size_x = arguments.sizex
     blok_size_y = arguments.sizey
     input_images = arguments.input
     output_name = arguments.output
+    opt = arguments.optimize
 
     if output_name[:-4] != '.tiff' or output_name[:-4] != '.tif':
         output_name += '.tif'
     max_ppi_x, max_ppi_y = img_sizer(input_images)
     output_image = make_image(blok_size_x, blok_size_y, max_ppi_x, max_ppi_y, input_images)
-    output_image.save(output_name, optimize=True)
+    output_image.save(output_name, optimize=opt)
 
 
 
